ae3ca7d1104d34a42d3a3f87b579078c45ca8974
Daniel JS <daniel@mydeveloperjourney.com>
2022-09-20 13:26:18 -0500

    wip: defines model and mounts middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // require dependenices
 const express = require('express');
 const mongoose = require('mongoose');
+const logger = require('morgan');
 // initialize the express app
 const app = express();
 // configuring application settings
@@ -17,8 +18,20 @@ mongoose.connection
 .on('disconnected', () => console.log('Disconnected from MongoDB'))
 .on('error', (error) => console.log('Problem with MongoDB: ' + error.message));
 
-// mount middleware
+// set up our model
+const peopleSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    title: String,
+}, { timestamps: true });
 
+const People = mongoose.model('People', peopleSchema);
+
+// mount middleware
+// app.use(express.urlencoded({ extended: false })) // intercepts incoming form data and turns it into req.body
+app.use(express.json()); 
+// ^ this middleware intercepts incoming json request bodies and turns them into req.body
+app.use(logger('dev'));
 // mount our routes
 app.get('/', (req, res) => {
     res.send('Welcome to the People Management App');
